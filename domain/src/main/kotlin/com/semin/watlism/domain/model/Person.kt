98e5f7e0c9ad59f3d74d2753efac0a6da899e30b
osemin <semin015123@gmail.com>
2025-08-17 23:34:50 +0900

    fix(domain) : move model to out of sealed interface

--- domain/src/main/kotlin/com/semin/watlism/domain/model/Person.kt
+++ domain/src/main/kotlin/com/semin/watlism/domain/model/Person.kt
@@ -6,21 +6,21 @@ sealed interface Person {
     val id: Long
     val name: String
     val profileUrl: Url?
-
-    data class Crew(
-        override val id: Long,
-        override val name: String,
-        override val profileUrl: Url?,
-        val job: CrewJob
-    ): Person
-
-    data class Actor(
-        override val id: Long,
-        override val name: String,
-        override val profileUrl: Url?,
-        val characterName: String
-    ): Person
 }
+
+data class Crew(
+    override val id: Long,
+    override val name: String,
+    override val profileUrl: Url?,
+    val job: CrewJob
+): Person
+
+data class Actor(
+    override val id: Long,
+    override val name: String,
+    override val profileUrl: Url?,
+    val characterName: String
+): Person
 
 enum class CrewJob {
     Director
--- domain/src/main/kotlin/com/semin/watlism/domain/model/Title.kt
+++ domain/src/main/kotlin/com/semin/watlism/domain/model/Title.kt
@@ -14,39 +14,39 @@ sealed interface Title {
     val createdAt: LocalDate
     val shortDescription: String
     val longDescription: String
-    val crews: List<Person.Crew>
-    val actors: List<Person.Actor>
-
-    data class Movie(
-        override val id: Long,
-        override val name: String,
-        override val posterUrl: Url,
-        override val rating: Rating,
-        override val voteCounts: Int,
-        override val createdAt: LocalDate,
-        override val shortDescription: String,
-        override val longDescription: String,
-        override val crews: List<Person.Crew>,
-        override val actors: List<Person.Actor>,
-        val runningTime: LocalTime,
-    ) : Title
-
-    data class Series(
-        override val id: Long,
-        override val name: String,
-        override val posterUrl: Url,
-        override val rating: Rating,
-        override val voteCounts: Int,
-        override val createdAt: LocalDate,
-        override val shortDescription: String,
-        override val longDescription: String,
-        override val crews: List<Person.Crew>,
-        override val actors: List<Person.Actor>,
-        val seasonCount: Int,
-        val episodeCount: Int,
-        val airingStatus: AiringStatus
-    ) : Title
+    val crews: List<Crew>
+    val actors: List<Actor>
 }
+
+data class Movie(
+    override val id: Long,
+    override val name: String,
+    override val posterUrl: Url,
+    override val rating: Rating,
+    override val voteCounts: Int,
+    override val createdAt: LocalDate,
+    override val shortDescription: String,
+    override val longDescription: String,
+    override val crews: List<Crew>,
+    override val actors: List<Actor>,
+    val runningTime: LocalTime,
+) : Title
+
+data class Series(
+    override val id: Long,
+    override val name: String,
+    override val posterUrl: Url,
+    override val rating: Rating,
+    override val voteCounts: Int,
+    override val createdAt: LocalDate,
+    override val shortDescription: String,
+    override val longDescription: String,
+    override val crews: List<Crew>,
+    override val actors: List<Actor>,
+    val seasonCount: Int,
+    val episodeCount: Int,
+    val airingStatus: AiringStatus
+) : Title
 
 enum class AiringStatus {
     OnAir, Ended
